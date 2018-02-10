c97782e5e0cddbcdd18ccccd1e0a57b8dc47a9c5
Inna Ki <innaki.kinevsky@gmail.com>
2018-02-10 03:16:53 +0200

    Python homework .

--- hmwrk3.py
+++ hmwrk3.py
@@ -3,7 +3,7 @@
 import datetime
 
 def calc_year(my_age):
-    year = 2018 + 120 - my_age 
+    year = datetime.date.today().year + 120 - my_age 
     return(year)
 
 name = input ('Please enter your name:')
