65282e2a0b9b27d19368dcbff8e8644b9de1cfd5
Inna Ki <innaki.kinevsky@gmail.com>
2018-02-10 03:26:10 +0200

    Python homework

--- hmwrk3.py
+++ hmwrk3.py
@@ -2,15 +2,10 @@
 
 import datetime
 
-def calc_year(my_age):
-    year = datetime.date.today().year + 120 - my_age 
-    return(year)
-
 name = input ('Please enter your name:')
 age = int (input ('Please enter your age:'))
 
 
-print (name)
-print (calc_year(age))
+print (name, "will be 120 in", datetime.date.today().year + 120 - age)
 
 
